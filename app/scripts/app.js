800f9ff1ed51dc33c8592b979c0a16254decdb75
awalsh <awalsh@nexapp.ca>
2016-01-16 15:58:05 -0500

    Added a method to compute percentage of task progression Fixed some lint issues

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,7 +9,8 @@
   // Sets app default base URL
   app.baseUrl = '/';
   if (window.location.port === '') {  // if production
-    // Uncomment app.baseURL below and set app.baseURL to '/your-pathname/' if running from folder in production
+    // Uncomment app.baseURL below and
+    // set app.baseURL to '/your-pathname/' if running from folder in production
     // app.baseUrl = '/polymer-starter-kit/';
   }
 
