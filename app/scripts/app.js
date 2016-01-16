49439d0d5c2a4faf4965d9ff252fefa98025d4c1
awalsh <awalsh@nexapp.ca>
2016-01-16 10:26:49 -0500

    Cleared some useless things

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,17 +9,9 @@
   // Sets app default base URL
   app.baseUrl = '/';
   if (window.location.port === '') {  // if production
-    // Uncomment app.baseURL below and
-    // set app.baseURL to '/your-pathname/' if running from folder in production
+    // Uncomment app.baseURL below and set app.baseURL to '/your-pathname/' if running from folder in production
     // app.baseUrl = '/polymer-starter-kit/';
   }
-
-  app.displayInstalledToast = function() {
-    // Check to make sure caching is actually enabled—it won't be in the dev environment.
-    if (!Polymer.dom(document).querySelector('platinum-sw-cache').disabled) {
-      Polymer.dom(document).querySelector('#caching-complete').show();
-    }
-  };
 
   // Listen for template bound event to know when bindings
   // have resolved and content has been stamped to the page
